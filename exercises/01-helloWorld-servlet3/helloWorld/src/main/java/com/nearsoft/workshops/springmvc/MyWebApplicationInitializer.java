3c4d684e265a38403662efd2229cbc2b2cf42c64
Rafael Gutierrez <abadon.gutierrez@gmail.com>
2015-09-15 00:02:23 -0500

    hello world with xml and without xml

---
+++ exercises/01-helloWorld-servlet3/helloWorld/src/main/java/com/nearsoft/workshops/springmvc/MyWebApplicationInitializer.java
@@ -0,0 +1,20 @@
+package com.nearsoft.workshops.springmvc;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class MyWebApplicationInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{ "/myapp/*" };
+    }
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{ MyWebConfig.class };
+    }
+}
