466ada44f198b7f3760ff2921a81a0253a7710ea
abadon.gutierrez <abadon.gutierrez@gmail.com>
2015-11-19 12:49:11 -0600

    Deleted solutions labs

--- labs/lab02-solution/src/main/java/com/nearsoft/workshops/springmvc/config/MyAppConfig.java
+++
@@ -1,9 +0,0 @@
-package com.nearsoft.workshops.springmvc.config;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@ComponentScan(basePackages = "com.nearsoft.workshops.springmvc.service")
-@Configuration
-public class MyAppConfig {
-}
